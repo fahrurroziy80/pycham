25b54de852baf2dcdd3debd79ce27e8a2d0c888c
Fahrurrozy <rrozif96@gmail.com>
2025-07-07 13:19:20 +0700

    ;(

---
+++ belajar/b1.py
@@ -0,0 +1,32 @@
+#
+#
+# angka1 = int(input("masukan angka 1 : "))
+# angka2 = int(input("masukan angka 2 : "))
+#
+# angka = angka1 + angka2
+# print("Hasilnya adalah =",angka)
+#
+
+def tambah():
+    angka = int(input("masukan angka :"))
+
+    if angka % 2 == 0:
+        print(f"{angka} bilangan genap ")
+    else:
+        print(f"{angka} bilangan ganjil ")
+
+
+tambah()
+tambah()
+tambah()
+# tambah()
+# tambah()
+
+
+
+# angka = int(input("Masukkan sebuah angka: "))
+#
+# if angka % 2 == 0:
+#     print(f"{angka} adalah bilangan genap.")
+# else:
+#     print(f"{angka} adalah bilangan ganjil.")
