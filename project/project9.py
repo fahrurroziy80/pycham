20b24d6914d3bd37a6ad03bcc2828e0282dd6de6
Fahrurrozy <rrozif96@gmail.com>
2025-07-08 21:20:46 +0700

    update

---
+++ project/project9.py
@@ -0,0 +1,51 @@
+import random
+import string
+import socket
+
+
+
+def program():
+    pilihan = input("====Pilih Opsi====\n"
+                    "[1] Generate password\n"
+                    "[2] IP generate\n"
+                    "[3] Kalkulator \n"
+                    "Masukan pilihan :")
+
+    if pilihan == '1':
+        def generate_password(panjang):
+            karakter = string.ascii_letters + string.digits + string.punctuation
+            password = ''.join(random.choice(karakter) for _ in range(panjang))
+            return password
+
+        panjang = int(input("Masukan panjang password : "))
+        print("password : ", generate_password(panjang))
+    elif pilihan == '2':
+        hostname = input("Masukkan nama host : ")
+        ip = socket.gethostbyname(hostname)
+        print(f"IP address dari {hostname} adalah :{ip}")
+    elif pilihan == '3':
+        def kalkulator():
+            a = float(input("masukan angka pertama :"))
+            b = float(input("masukan angka kedua : "))
+
+            operator = input("pilih operasi : [+ , - , * , / ] : ")
+
+            if operator == "+":
+                hasil = a + b
+            elif operator == "-":
+                hasil = a - b
+            elif operator == "*":
+                hasil = a * b
+            elif operator == "/":
+                hasil = a / b
+            else:
+                print("oprasi tidak valid ")
+                return
+
+            print("adalah", hasil)
+
+        kalkulator()
+    else:
+        print("program berakhir ")
+        return
+program()
