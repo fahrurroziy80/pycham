24e2e79d7f3971f6a3269a1fa2675a2f9fa1ba86
Fahrurrozy <rrozif96@gmail.com>
2025-07-09 10:08:25 +0700

    b2

---
+++ belajar/b2.py
@@ -0,0 +1,26 @@
+def makanan():
+
+    menu = input("[1] ayam gorang \n"
+                 "[2] telor goreng \n"
+                 "[3] gorengan \n"
+                 "[4] mie ayam \n"
+                 "pilih makanan :")
+    if menu == '1':
+        print("ayam gorang")
+    elif menu == '2':
+        print("telor goreng")
+    elif menu == '3':
+        print("gorengan")
+    elif menu == '4':
+        print("mie ayam ")
+    else:
+        print("")
+        return
+
+makanan()
+print("terimakasih sudah memesan makanan di sini :)")
+
+
+
+
+
