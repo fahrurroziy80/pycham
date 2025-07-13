0b758c3a336e7e0ed50bc1f1cfe40473fcb4d4be
Fahrurrozy <rrozif96@gmail.com>
2025-07-13 14:03:06 +0700

    =

---
+++ belajar/b7.py
@@ -0,0 +1,30 @@
+# def fibonacci(n):
+#     if n <= 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return fibonacci(n-1) + fibonacci(n-2)
+#
+# # Contoh penggunaan
+# n = 10
+# for i in range(n):
+#     print(fibonacci(i), end=" ")
+
+hasil = []
+
+for i in range(5):
+    hasil.append(1 *  5)
+
+hasil = [i * 2 for i in range(5)]
+
+
+
+print(hasil)  # Output: [0, 2, 4, 6, 8]
+
+for j in range(5):
+    hasil.append(i % 10)
+
+hasil1 = [j % 2 for j in range(5)]
+
+print(hasil1)
