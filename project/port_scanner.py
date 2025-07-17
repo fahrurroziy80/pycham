dc8e7e656d52e1e60d0b6c81ef8e2f82c3c47810
Fahrurrozy <rrozif96@gmail.com>
2025-07-17 22:50:43 +0700

    update +nama layanan

--- project/port_scanner.py
+++ project/port_scanner.py
@@ -1,22 +1,33 @@
 import socket
 
 
+def get_service(port):
+    try:
+        return socket.getservbyport(port)
+    except:
+        return "Unknown Service"
+
+
 def port_scanner(ip, start_port, end_port):
-    print(f"Scanning {ip} dari port {start_port} sampai {end_port}...\n")
+    print(f"\n🔎 Scanning {ip} dari port {start_port} sampai {end_port}...\n")
 
     for port in range(start_port, end_port + 1):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(0.5)  # Biar gak nunggu lama
-        result = s.connect_ex((ip, port))  # 0 = berhasil terhubung (port terbuka)
+        s.settimeout(0.5)
+
+        result = s.connect_ex((ip, port))
 
         if result == 0:
-            print(f"[+] Port {port} TERBUKA")
+            service = get_service(port)
+            print(f"[+] Port {port} TERBUKA → Layanan: {service}")
+
         s.close()
 
 
-
+# Input dari pengguna
 target_ip = input("Masukkan IP target: ")
 start = int(input("Dari port: "))
 end = int(input("Sampai port: "))
 
+# Jalankan scanner
 port_scanner(target_ip, start, end)
