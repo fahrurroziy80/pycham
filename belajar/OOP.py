57cb790dc694883e5ead1c3df9ce580ed7353354
Fahrurrozy <rrozif96@gmail.com>
2025-08-05 13:53:35 +0700

    OOP

--- belajar/OOP.py
+++ belajar/OOP.py
@@ -13,3 +13,35 @@ mhs2 = Mahasiswa("Dewi", "654321")
 # Memanggil method
 mhs1.perkenalan()
 mhs2.perkenalan()
+
+print("=====================================================")
+print("=====================================================")
+print("=====================================================")
+
+class ATM:
+    def __init__(self, saldo_awal):
+        self.saldo = saldo_awal
+
+    def nabung(self, jumlah):
+        self.saldo += jumlah
+        print(f"Berhasil menabung Rp{jumlah:,}. Saldo sekarang: Rp{self.saldo:,}")
+
+    def tarik_saldo(self, jumlah):
+        if jumlah > self.saldo:
+            print("Saldo tidak cukup untuk penarikan!")
+        else:
+            self.saldo -= jumlah
+            print(f"Berhasil menarik Rp{jumlah:,}. Saldo sekarang: Rp{self.saldo:,}")
+
+    def cek_saldo(self):
+        print(f"Saldo anda saat ini: Rp{self.saldo:,}")
+
+# Membuat object ATM
+akun1 = ATM(1000000)
+
+# Simulasi transaksi
+akun1.cek_saldo()
+akun1.nabung(500000)
+akun1.tarik_saldo(300000)
+akun1.tarik_saldo(1500000)  # Ini harus gagal karena saldo kurang
+akun1.cek_saldo()
